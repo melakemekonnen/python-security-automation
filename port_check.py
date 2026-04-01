b11aa701a0f6770500f4c5ed2a706bf23ad91a97
Melake <melakemekonnen24@gmail.com>
2026-04-01 02:43:56 -0400

    sec: completed session 07 port checker built

---
+++ port_check.py
@@ -0,0 +1,32 @@
+import socket
+
+# Open the target file, extract IPs into a list, and replace the gateway 
+with open("/home/melakee/lab_prep/target_intel.txt", "r") as f:
+	targets = [line.strip().split(": ")[1] for line in f.readlines()]
+
+my_ip="192.168.64.1"
+targets[-1]=my_ip
+
+
+targets = ["127.0.0.1", "8.8.8.8", "1.1.1.1", "10.0.0.1", "192.168.64.1"]
+for ip in targets:
+    print(f"--- Checking Server: {ip} ---")
+
+    # Create a socket to knock on the door
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Set a timer so we don't wait forever
+    s.settimeout(1)
+
+    # Knock on Port 22 (SSH)
+    result = s.connect_ex((ip, 22))
+
+    # 0 means Open, anything else means Closed
+    if result == 0:
+        print(f"SUCCESS: Port 22 is OPEN on {ip}")
+    else:
+        print(f"FAILED: Port 22 is CLOSED on {ip}")
+
+    # Close the connection
+    s.close() 
+
